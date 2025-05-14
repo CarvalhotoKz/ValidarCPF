4c8ae8c99818335ee37f821c70719902e73b8082
CarvalhotoKz <silva.joaovictorcarvalho@gmail.com>
2025-05-13 22:19:17 -0300

    Finalização da validação de CPF em JS

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function validarCPF(cpf){
-    cpf = cpf.replace(/[^\d]+/g, "");
+    cpf = cpf.replace(/[^\d]+/g,"");
 
-    if(cpf.lenght !== 11 || /^(\d)\1+$/.test(cpf)){
+    if(cpf.length !== 11 || /^(\d)\1+$/.test(cpf)){
         return false;
     }
 
@@ -10,7 +10,7 @@ function validarCPF(cpf){
 
     // Validação do 1º DV
     for(let i=1; i <= 9; i++){
-        soma += parseInt(cpf.subtring(i-1, i)) * (11-i);
+        soma += parseInt(cpf.substring(i-1, i)) * (11-i);
     }
 
     resto = (soma * 10) % 11;
@@ -19,5 +19,44 @@ function validarCPF(cpf){
         resto = 0;
     }
 
+    if(resto !== parseInt(cpf.substring(9, 10))){
+        return false;
+    }
+
+    // Validação do 2º DV
+
+    soma = 0;
+    for(let i = 1; i <= 10; i++){
+        soma += parseInt(cpf.substring(i-1, i)) * (12 - i);
+    }
+
+    resto = (soma * 10) % 11;
+
+    if(resto === 10 || resto === 11){
+        resto = 0;
+    }
+    if(resto !== parseInt(cpf.substring(10, 11))){
+        return false;
+    }
+
+    return true;
+
 
 }
+
+document.getElementById("cpfForm").addEventListener("submit", function(e){
+   e.preventDefault();
+   const cpfInput = document.getElementById("cpf").value;
+   const messageDiv = document.getElementById("message");
+
+   if(validarCPF(cpfInput)){
+    messageDiv.textContent = "CPF válido";
+    messageDiv.className = "message success";
+   } else{
+    messageDiv.textContent = "CPF Inválido";
+    messageDiv.className = "message error";
+   }
+
+
+   messageDiv.style.display = "block";
+});
